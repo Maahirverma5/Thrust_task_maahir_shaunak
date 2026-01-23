4d90e50330e21b880568b6c8cd1aff42107cdae9
Maahirverma5 <maahirverma5@gmail.com>
2026-01-23 17:30:37 +0530

    Lower confidence thresholds for hand detection

--- twoplayerversion.py
+++ twoplayerversion.py
@@ -42,8 +42,8 @@ bomb_sound = pygame.mixer.Sound("bomb.mp3")
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(
     max_num_hands=2,
-    min_detection_confidence=0.7,
-    min_tracking_confidence=0.7
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
 )
 
 # ===============================
